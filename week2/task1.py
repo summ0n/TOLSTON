d21d59ada24451c7a20f9eb9aa1a66566b48828f
summ0n <dreadn0ut9a@yandex.ru>
2019-03-04 22:29:34 +0300

    q2

---
+++ week2/task1.py
@@ -0,0 +1,23 @@
+AGE = None
+def get_age():
+    AGE = None
+    while not type(AGE) == int or float:
+        try:
+            AGE = int(input("Введите ваш возраст(используйте числа!!!):"))
+        except ValueError:
+            print("ИСПОЛЬЗУЙ ТОЛЬКО ЧИСЛА")
+            print(type(AGE))
+
+
+    return AGE
+
+def age_counter():
+    AGE = get_age()
+    while AGE < 0:
+        print("Возраст не может быть отрицательным")
+        AGE = get_age()
+    if AGE :
+        if AGE > 0 and AGE < 7:
+            print("Привет малыш, собирайся в детский сад")
+
+age_counter()        
