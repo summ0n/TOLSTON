9a4ef66a7b32ccb5d063f6cc5fc79d6e00b8631b
summ0n <dreadn0ut9a@yandex.ru>
2019-03-11 21:34:14 +0300

    while loop

--- week2/task1.py
+++ week2/task1.py
@@ -1,22 +1,37 @@
-AGE = None
-def get_age():
-    AGE = None
-    while not type(AGE) == int:
-        try:
-            AGE = int(input("Введите ваш возраст(используйте числа!!!):"))
-        except ValueError:
-            print("ИСПОЛЬЗУЙ ТОЛЬКО ЧИСЛА")
+def age_dist(age):
+    try:
+        age = float(age)
+    except ValueError:
+        print("Используй числа.")
+        return True
+    if age < 0:
+        print("Не может быть отрицательным.")
+        return True
+    elif age > 0 and age <= 6:
+        print("Ты должен идти в детский сад.")
+        return False
+    elif age > 6 and age <= 18:
+        print("Ты должен идти в школу.")
+        return False
+    elif age > 18 and age <= 23:
+        print("Ты должен идти в институт.")
+        return False
+    elif age > 23 and  age <= 60:
+        print("Ты должен работать")
+        return False
+    elif age > 60:
+        print("Ты на пенсии.")
+        return False
+    else:
+        print("WTF (age)?")
+        return False
 
+while True:
+    myage = input("Введите ваш возраст(используйте числа!!!):")
+    print(myage)
+    if myage == "/stop":
+        break
+    result = age_dist(myage)
+    if result == False:
+        break
 
-    return AGE
-
-def age_counter():
-    AGE = get_age()
-    while AGE < 0:
-        print("Возраст не может быть отрицательным")
-        AGE = get_age()
-    if AGE :
-        if AGE > 0 and AGE < 7:
-            print("Привет малыш, собирайся в детский сад")
-
-age_counter()        
